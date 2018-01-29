9ecf293ce133ff9b991f72a4925f34b297cf9216
Eugene Krashtan <eugene.krashtan@anoto.com>
2018-01-29 18:01:54 +0200

    test3

--- Android/r0b1c/app/src/main/java/com/krashtan/eug/r0b1c/BluetoothHandler.java
+++ Android/r0b1c/app/src/main/java/com/krashtan/eug/r0b1c/BluetoothHandler.java
@@ -2,19 +2,30 @@ package com.krashtan.eug.r0b1c;
 
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BluetoothHandler {
     private Context context;
     private boolean mEnabled = false;
     private BluetoothAdapter mBluetoothAdapter;
     private String mCurrentConnectedBLEAddr;
+    private BLEDeviceListAdapter mDevListAdapter;
 
     public BluetoothHandler(Context context) {
         this.context = context;
+        mDevListAdapter = new BLEDeviceListAdapter(context);
         mBluetoothAdapter = null;
         mCurrentConnectedBLEAddr = null;
 
@@ -54,5 +65,81 @@ public class BluetoothHandler {
 
     public boolean isEnabled(){
         return mEnabled;
+    }
+
+    public BLEDeviceListAdapter getDeviceListAdapter(){
+        return mDevListAdapter;
+    }
+
+    // scan device
+    public void scanLeDevice(boolean enable) {
+        if (enable) {
+
+        } else {
+
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                             final byte[] scanRecord) {
+
+        }
+    };
+
+    public class BluetoothScanInfo{
+        public BluetoothDevice device;
+        public int rssi;
+    };
+
+    public class BLEDeviceListAdapter extends BaseAdapter {
+        private Context context;
+        private TextView devNameTextView, devRssiTextView;
+        private ArrayList<BluetoothScanInfo> bleArrayList;
+
+        public BLEDeviceListAdapter(Context context) {
+            this.context = context;
+            bleArrayList = new ArrayList<BluetoothScanInfo>();
+        }
+
+        @Override
+        public int getCount() {
+            return bleArrayList.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public BluetoothScanInfo getItem(int position) {
+            return bleArrayList.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout layout = null;
+
+            if (convertView == null) {
+                layout = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.item_list, null);
+                devNameTextView = (TextView) layout.findViewById(R.id.textViewDevName);
+                devRssiTextView = (TextView) layout.findViewById(R.id.textViewRssi);
+                convertView = layout;
+            }
+
+            BluetoothDevice device = bleArrayList.get(position).device;
+            String devName = device.getName();
+            int rssi = bleArrayList.get(position).rssi;
+
+            if (devName != null && devName.length() > 0) {
+                devNameTextView.setText(devName);
+            } else {
+                devNameTextView.setText("unknow-device");
+            }
+            devRssiTextView.setText(String.valueOf(rssi));
+            return convertView;
+        }
     }
 }
--- Android/r0b1c/app/src/main/java/com/krashtan/eug/r0b1c/MainActivity.java
+++ Android/r0b1c/app/src/main/java/com/krashtan/eug/r0b1c/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private BluetoothHandler bluetoothHandler;
+    private ListView bleDeviceListView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        bluetoothHandler = new BluetoothHandler(this);
+        bleDeviceListView = (ListView) findViewById(R.id.bleDeviceListView);
     }
 
+    public void scanOnClick(final View v){
+        bleDeviceListView.setAdapter(bluetoothHandler.getDeviceListAdapter());
+        bluetoothHandler.scanLeDevice(true);
+    }
+
+    private void showMessage(String str){
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
 }
